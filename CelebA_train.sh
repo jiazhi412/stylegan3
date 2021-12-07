dbe123ac09615e598ed1f16b3a0b062c1755b07f
Jiazhi Li <jiazhil@usc.edu>
2021-12-07 15:50:38 -0800

    Stylegan3-r 1 gpu and 8 gpus running (new start)

--- CelebA_train.sh
+++ CelebA_train.sh
@@ -1,8 +1,8 @@
 # 1 gpu
-python train.py --outdir=./training-runs/CelebA --data=./datasets/CelebA/ --cfg stylegan3-t --gpus=1 --batch=32 --gamma=2 --batch-gpu=16
+python train.py --outdir=./training-runs/CelebA --data=./datasets/CelebA/ --cfg=stylegan3-r --gpus=1 --batch=32 --gamma=2 --batch-gpu=4
 
 # 8 gpus
-python train.py --outdir=./training-runs/CelebA --data=./datasets/CelebA/ --cfg stylegan3-t --gpus=8 --batch=32 --gamma=2
+python train.py --outdir=./training-runs/CelebA --data=./datasets/CelebA/ --cfg=stylegan3-r --gpus=8 --batch=32 --gamma=2 --batch-gpu=1 --mbstd-group=1
 
 # resume
 python train.py --outdir=./training-runs/CelebA --data=./datasets/CelebA/ --resume ./training-runs/CelebA/
